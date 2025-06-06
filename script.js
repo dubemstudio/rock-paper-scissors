d79614f512a0487630978484d8fb2f9819725331
Dubem Studio <info.dubemstudio@gmail.com>
2025-06-06 03:36:45 +0100

    worked on the toggle theme

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-/* ----------------------------------------------- */
-/* Set view height*/
-function setVH() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setVH();
-window.addEventListener('resize', setVH);
-/* ----------------------------------------------- */
-
-
-/* ----------------------------------------------- */
-/* Toggle Theme */
-const toggleBtn = document.getElementById('toggleBtn');
-const body = document.querySelector('body');
-const illustration = document.getElementById('illustration');
-
-let darkmode = localStorage.getItem('darkmode');
-
-const enableDarkmode = () => {
-  document.body.classList.add('darkmode');
-  localStorage.setItem('darkmode', 'active');
-  illustration.src = "image/illustrationGroupDark.svg";
-}
-const disableDarkmode = () => {
-  document.body.classList.remove('darkmode');
-  localStorage.removeItem('darkmode');
-  illustration.src = "image/illustrationGroupLight.svg";
-}
-
-if(darkmode === "active"){
-  enableDarkmode();
-} else {
-  disableDarkmode();
-}
-
-toggleBtn.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode');
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
-})
+  /* ----------------------------------------------- */
+  /* Set view height*/
+  function setVH() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  setVH();
+  window.addEventListener('resize', setVH);
+  /* ----------------------------------------------- */
+  
+  
+  /* ----------------------------------------------- */
+  /* Toggle Theme */
+  const toggleBtn = document.getElementById('toggleBtn');
+  const body = document.querySelector('body');
+  const illustration = document.getElementById('illustration');
+  
+  let darkmode = localStorage.getItem('darkmode');
+  
+  const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+    if(illustration){
+      illustration.src = "image/illustrationGroupDark.svg";
+    }
+  }
+  const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.removeItem('darkmode');
+    if(illustration){
+      illustration.src = "image/illustrationGroupLight.svg";
+    }
+  }
+  
+  if(darkmode === "active"){
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+  if(toggleBtn){
+    toggleBtn.addEventListener("click", () => {
+      darkmode = localStorage.getItem('darkmode');
+      darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+    })
+  }
